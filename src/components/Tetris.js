12551280ea7610bc7de87c7c560c228f0127de6d
Pirun Seng <sengpirun.rain@gmail.com>
2020-10-07 14:54:41 +0700

    drop with useInterval

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { checkCollision, createStage } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -32,8 +33,9 @@ const Tetris = () => {
   const startGame = () => {
     console.log("test")
     // Reset everything
-    setStage(createStage());
-    resetPlayer();
+    setStage(createStage())
+    setDropTime(1000)
+    resetPlayer()
     setGameOver(false)
   }
 
@@ -51,8 +53,19 @@ const Tetris = () => {
     }
   }
 
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000) // when user releasing the down key
+        console.log("------------- interval ON")
+      }
+    }
+  }
+
   const dropPlayer = () => {
-    drop();
+    setDropTime(null) // stop interval when user pressing down key
+    console.log("interval OFF")
+    drop()
   }
 
   const move = ({ keyCode }) => {
@@ -70,8 +83,13 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop()
+
+  }, dropTime)
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={e => keyUp(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
